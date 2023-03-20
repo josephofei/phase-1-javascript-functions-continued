0f5bec25adb43737ee45cdff4f74e39e16a2d7a2
Joseph Ofei <josephofei24@gmail.com>
2023-03-20 17:17:02 -0400

    function lab continue complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // code your solution here
+
+function saturdayFun(game = 'roller-skate') {
+    return `This Saturday, I want to ${game}!`
+}
+
+const mondayWork = function (work = 'go to the office') {
+    return `This Monday, I will ${work}.`
+}
+
+
+function wrapAdjective(string1) {
+    return function inner(compliment = 'special') {
+        compliment = `${string1}${compliment}${string1}`
+
+        // `*compliment*`
+        let declaration = `You are ${compliment}!`
+        /*     let result1 = wrapAdjective('*');
+             let result2 = wrapAdjective('||');
+             function name(string2) {
+                 if (result1 === true) {
+                     return function (string) {
+                         return `You are ${string}!`
+                     }
+                 }
+                 else if (result2 === true) {
+                     return function (string) {
+                         return `You are ${string}!`
+                     }
+                 }*/
+        return declaration
+    }
+}
+
+
